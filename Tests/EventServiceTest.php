af9c11d357ec8eeadebb1d55f57adaed7176701a
Damian <damian.piela@tmsolution.pl>
2015-08-19 14:55:31 +0200

    Merge branch 'master' of https://github.com/TMSolution/GamificationBundle
    Conflicts:
    	Service/EventService.php

--- Tests/EventServiceTest.php
+++ Tests/EventServiceTest.php
@@ -9,7 +9,7 @@
 namespace TMSolution\GamificationBundle\Tests;
 
 use TMSolution\GamificationBundle\Entity\Objectinstance;
-use TMSolution\GamificationBundle\Entity\Trophycategory;
+use TMSolution\GamificationBundle\Entity\Trophycategory; 
 use TMSolution\GamificationBundle\Entity\Trophy;
 use TMSolution\GamificationBundle\Entity\Objecttrophy;
 use TMSolution\GamificationBundle\Entity\Context;
