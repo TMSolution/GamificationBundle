2d4fe630bcab71027b8c24522c71b8581ae04399
Damian <damian.piela@tmsolution.pl>
2015-08-20 12:24:42 +0200

    Merge branch 'master' of https://github.com/TMSolution/GamificationBundle
    Conflicts:
    	Service/EventService.php

--- DataFixtures/ORM/LoadDataToDB.php
+++ DataFixtures/ORM/LoadDataToDB.php
@@ -1,14 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of LoadDataToDB
- *
+ * Loads test data to database
+ * 
+ * @author Damian Piela
  * @author Lukasz
  */
 
@@ -17,27 +12,66 @@ namespace TMSolution\GamificationBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use TMSolution\GamificationBundle\Entity\Context;
+use TMSolution\GamificationBundle\Entity\Trophytype;
+use TMSolution\GamificationBundle\Entity\Trophycategory;
 use Faker\Factory;
+use Core\ModelBundle\Model\Model;
 
 class LoadDataToDB implements FixtureInterface {
-//
-//    //put your code here
-//    public function load(ObjectManager $manager) {
-//        $faker = Factory::create();
-//
-//        for ($i = 0; $i < 10; $i++) {
-//
-//            for ($i = 0; $i < 1000; $i++) {
-//
-//                $context = new Context();
-//                $context->setName($faker->name);
-//
-//                $manager->persist($context);
-//            }
-//            $manager->flush();
-//        }
-//    }
+
+    public function load(ObjectManager $manager) {
+        //Faker
+        $faker = Factory::create();
+        $model = new Model();
+        $modelFactory = $model->getModelFactory('model_factory');
+
+        //Trophytype
+        $trophyType = new Trophytype();
+        $trophyType->setName('Jednorazowa');
+        $trophyType1 = new Trophytype();
+        $trophyType1->setName('Cykliczna');
+        $manager->persist($trophyType);
+        $manager->persist($trophyType1);
+        $manager->flush();
+
+
+        //Trophycategory
+        $trophyCategory = new Trophycategory();
+        $trophyCategory->setName('Kategoria testowa');
+        $manager->persist($trophyCategory);
+        $manager->flush();
+
+
+        //Trophy
+        $trophyCategoryModel = $modelFactory->getModel('TMSolution\GamificationBundle\Entity\Trophycategory');
+        $trophyTypeModel = $modelFactory->getModel('TMSolution\GamificationBundle\Entity\Trophytype');
+        $randTrophyType = rand(1, 2);
+
+        for ($i = 0; i < 10; $i++) {
+            $trophy = new Trophy();
+            $trophy->setName($faker->word)
+                    ->setImage($faker->ipv6)
+                    ->setTrophycategory($trophyCategoryModel->findOneById(1))
+                    ->setTrophytype($trophyTypeModel->findOneById($randTrophyType));
+            $manager->persist($trophy);
+        }
+        $manager->flush();
+
+        
+        
+        
+
+        for ($i = 0; $i < 10; $i++) {
+
+            for ($i = 0; $i < 1000; $i++) {
+
+                $context = new Context();
+                $context->setName($faker->name);
+
+                $manager->persist($context);
+            }
+            $manager->flush();
+        }
+    }
 
 }
-
-
